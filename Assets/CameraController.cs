d2da1379371a9242fabf0756412d838efce66d1c
Val <valgebo@gmail.com>
2024-04-09 15:17:35 +0200

    test

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public float height;
     public float width;
+    public float verticalRotation = 45f; // Fixed vertical rotation angle
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update camera position and rotation
         this.transform.position = player.transform.position + Vector3.up * height + Vector3.back * width;
+        this.transform.eulerAngles = new Vector3(verticalRotation, this.transform.eulerAngles.y, this.transform.eulerAngles.z);
     }
 }
